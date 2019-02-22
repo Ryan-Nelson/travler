7c459055930423131f08c066af1a38b00ef2b258
Ryan Nelson <ryannelson881995@gmail.com>
2019-02-22 16:45:30 -0600

    adding what i have

---
+++ README.md
@@ -0,0 +1,1 @@
+https://dbdiagram.io/d/5c658ebef7c5bb70c72f0590
---
+++ src/lib/grunt/bgShell.js
@@ -0,0 +1,8 @@
+module.exports = {
+  launchAPI: {
+      cmd: "json-server -p 9999 -w ../../api/database.json"
+  },
+  _defaults: {
+      bg: true
+  }
+};
